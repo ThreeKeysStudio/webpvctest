682d74ccb5a14ac44c50b8c410b5713df545268c
PORTATIL-PEDR\pedro <trollgamer125@gmail.com>
2025-01-26 15:43:27 +0100

    Mas pruebas tlf

--- js/home-code.js
+++ js/home-code.js
@@ -3,16 +3,12 @@ function toggleContactBox() {
   const button = document.getElementById("contact-button");
 
   // Alternar visibilidad del recuadro de contacto
-  if (contactBox.classList.contains("hidden")) {
+  if (contactBox.classList.contains("hidden") || contactBox.classList.contains("hidden_first") ) {
     contactBox.classList.remove("hidden");
+	contactBox.classList.remove("hidden_first");
     contactBox.classList.add("show"); // Añade la clase de animación
   } else {
     contactBox.classList.remove("show");
     contactBox.classList.add("hidden"); // Oculta el recuadro
   }
-
-  // Posicionar el recuadro de contacto justo encima y centrado con el botón
-  /*const buttonRect = button.getBoundingClientRect();
-  contactBox.style.top = `${buttonRect.top + window.scrollY - contactBox.offsetHeight}px`;
-  contactBox.style.left = `${buttonRect.left + window.scrollX + buttonRect.width / 2 - contactBox.offsetWidth / 2}px`;*/
 }
